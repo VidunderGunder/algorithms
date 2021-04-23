1382a20e4c725c6e9ec4e7b90ebbe33c0cbe012f
Kristian Gunder Kramås <kristiankramas@outlook.com>
2021-04-24 00:36:35 +0200

    Title to number started

---
+++ src/tasks/titleToNumber.ts
@@ -0,0 +1,18 @@
+function titleToNumber(columnTitle: string): number {
+  const num = 0;
+
+  for (let i = 0; i < columnTitle.length; i++) {
+    // TODO
+  }
+
+  return num;
+}
+
+console.log(titleToNumber("A")); // 1
+console.log(titleToNumber("B")); // 2
+console.log(titleToNumber("Z")); // 26
+console.log(titleToNumber("AA")); // 27
+console.log(titleToNumber("AB")); // 28
+
+console.log("A".charCodeAt(0));
+console.log("Z".charCodeAt(0));
