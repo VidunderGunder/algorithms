00841067b9d95219750b38a3df63b708a1bdac15
VidunderGunder <kristiankramas@outlook.com>
2021-04-27 19:31:05 +0200

    Unscoped floodFill

--- quokka/tasks/floodFill.ts
+++ quokka/tasks/floodFill.ts
@@ -1,42 +1,40 @@
-{
-  // https://leetcode.com/problems/flood-fill/
+// https://leetcode.com/problems/flood-fill/
 
-  function floodFill(
-    image: number[][],
-    sr: number,
-    sc: number,
-    newColor: number,
-    firstColor = image[sr][sc]
-  ): number[][] {
-    if (
-      sr < 0 ||
-      sc < 0 ||
-      sr >= image.length ||
-      sc >= image[0].length ||
-      image[sr][sc] !== firstColor ||
-      image[sr][sc] === newColor
-    )
-      return image;
-
-    image[sr][sc] = newColor;
-
-    floodFill(image, sr - 1, sc, newColor, firstColor);
-    floodFill(image, sr, sc + 1, newColor, firstColor);
-    floodFill(image, sr + 1, sc, newColor, firstColor);
-    floodFill(image, sr, sc - 1, newColor, firstColor);
-
+function floodFill(
+  image: number[][],
+  sr: number,
+  sc: number,
+  newColor: number,
+  firstColor = image[sr][sc]
+): number[][] {
+  if (
+    sr < 0 ||
+    sc < 0 ||
+    sr >= image.length ||
+    sc >= image[0].length ||
+    image[sr][sc] !== firstColor ||
+    image[sr][sc] === newColor
+  )
     return image;
-  }
 
-  const imageA = [
-    [1, 1, 1],
-    [1, 1, 0],
-    [1, 0, 1],
-  ];
-  console.log(floodFill(imageA, 1, 1, 2));
+  image[sr][sc] = newColor;
 
-  let imageB = new Array(50).fill(new Array(50).fill(1));
+  floodFill(image, sr - 1, sc, newColor, firstColor);
+  floodFill(image, sr, sc + 1, newColor, firstColor);
+  floodFill(image, sr + 1, sc, newColor, firstColor);
+  floodFill(image, sr, sc - 1, newColor, firstColor);
 
-  console.log(imageB);
-  console.log(floodFill(imageB, 1, 1, 2));
+  return image;
 }
+
+const imageA = [
+  [1, 1, 1],
+  [1, 1, 0],
+  [1, 0, 1],
+];
+console.log(floodFill(imageA, 1, 1, 2));
+
+let imageB = new Array(50).fill(new Array(50).fill(1));
+
+console.log(imageB);
+console.log(floodFill(imageB, 1, 1, 2));
