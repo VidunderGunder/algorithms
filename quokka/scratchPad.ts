31bc4aecfbdfbbee3d67524a6ca4021bdb1e6a20
VidunderGunder <kristiankramas@outlook.com>
2021-04-28 09:29:00 +0200

    Scratch Pad

---
+++ quokka/scratchPad.ts
@@ -0,0 +1,12 @@
+{
+  /**
+   * #### Scratch pad for experimenting
+   *
+   * Write code inside scope to avoid most conflicts
+   * with the rest of the project
+   */
+  const hoverToSeeMarkdown = () => {};
+  hoverToSeeMarkdown();
+
+  // Code here
+}
